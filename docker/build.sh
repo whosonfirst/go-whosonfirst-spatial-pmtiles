2d86b2a571507a582367c2ced17a2bf4216f406b
sfomuseumbot <sfomuseumbot@localhost>
2022-12-05 11:22:45 -0800

    update to allow custom -spr properties

--- docker/build.sh
+++ docker/build.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-SOURCE=""	# for example: 'sfomuseum-data://?prefix=sfomuseum-data-whosonfirst'
-NAME=""		# for example whosonfirst"
-TARGET=""	# for example: s3blob://bucket?region=us-east-1&credentials=iam:
+SOURCE=""	# for example: -s 'sfomuseum-data://?prefix=sfomuseum-data-whosonfirst'
+NAME=""		# for example -n whosonfirst"
+TARGET=""	# for example: -t s3blob://bucket?region=us-east-1&credentials=iam:
 
 ITERATOR="org:///tmp"
 ZOOM="12"
 
-while getopts "i:n:s:t:z:" opt; do
+PROPERTIES=""	# for example: -p 'wof:hierarchy wof:concordances'
+
+while getopts "i:n:p:s:t:z:" opt; do
     case "$opt" in
 	i)
 	    ITERATOR=$OPTARG
 	    ;;
 	n)
 	    NAME=$OPTARG
+	    ;;
+	p)
+	    PROPERTIES=$OPTARG
 	    ;;
 	s )
 	    SOURCE=$OPTARG
@@ -32,13 +37,14 @@ done
 
 echo "Import ${SOURCE} FROM ${ITERATOR} as ${NAME} and copy to ${TARGET}"
 
-wof-tippecanoe-features \
-    -as-spr \
-    -require-polygons \
-    -spr-append-property wof:hierarchy \
-    -writer-uri 'constant://?val=jsonl://?writer=stdout://' \
-    -iterator-uri ${ITERATOR} ${SOURCE} \
-    | tippecanoe -P -z ${ZOOM} -pf -pk -o /usr/local/data/${NAME}.mbtiles
+FEATURES_ARGS="-as-spt -require-polygons -writer-uri 'constant://?val=jsonl://?writer=stdout://' -iterator-uri ${ITERATOR} ${SOURCE}"
+
+for PROP in ${PROPERTIES}
+do
+    FEATURES_ARGS="${FEATURES_ARGS} -spr-append-property ${PROP}"
+done
+
+wof-tippecanoe-features ${FEATURE_ARGS} | tippecanoe -P -z ${ZOOM} -pf -pk -o /usr/local/data/${NAME}.mbtiles
 
 pmtiles convert /usr/local/data/${NAME}.mbtiles /usr/local/data/${NAME}.pmtiles
 
