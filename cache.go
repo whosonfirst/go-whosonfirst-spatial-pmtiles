d097458807787233773ee308b1c571b329e7841c
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2022-12-08 10:46:24 -0800

    update to use json-iterator/go as a custom marshal/unmarshal -er for orb/geojson (#3)

--- cache.go
+++ cache.go
@@ -18,7 +18,8 @@ const FEATURES_CACHE_TABLE string = "pmtiles_features"
 
 // JSON-encoding features is not ideal given the known performance issues around marshaling
 // and unmarshaling JSON but everything else fails at some stage with type issues so this will
-// do for now.
+// do for now. Note that we are using json-iterator/go as a orb/geojson custom marshaler/unmarsheler
+// whic his defined in pmtiles.go
 
 type FeatureCache struct {
 	Created int64  `json:"created"`
