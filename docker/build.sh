b970797b73c0e0df5e66a3fa1cca34dffcd83281
sfomuseumbot <sfomuseumbot@localhost>
2023-01-09 10:49:43 -0800

    snapshot: start implementing changes to use tippecanoe 2.17 to produce pmtiles directly

--- docker/build.sh
+++ docker/build.sh
@@ -3,14 +3,23 @@
 SOURCES=""		# for example: -s 'sfomuseum-data://?prefix=sfomuseum-data-whosonfirst'
 TARGET=""		# for example: -t s3blob://bucket?region=us-east-1&credentials=iam:
 
+WRITE_FEATURES=""
+HELP=""
+
 NAME="whosonfirst"	# for example -n whosonfirst"
 ITERATOR="org:///tmp"
 ZOOM="12"
 
 PROPERTIES=""	# for example: -p 'wof:hierarchy wof:concordances'
 
-while getopts "i:n:p:s:t:z:" opt; do
+while getopts "i:n:p:s:t:z:fh" opt; do
     case "$opt" in
+	f)
+	    WRITE_FEATURES=1
+	    ;;
+	h)
+	    HELP=1
+	    ;;
 	i)
 	    ITERATOR=$OPTARG
 	    ;;
@@ -35,6 +44,12 @@ while getopts "i:n:p:s:t:z:" opt; do
     esac
 done
 
+if [ "${HELP}" = "1" ]
+then
+    echo "Print this message"
+    exit 0
+fi
+
 echo "Import ${SOURCE} FROM ${ITERATOR} as ${NAME} and copy to ${TARGET}"
 
 FEATURES_ARGS="-as-spr -require-polygons -writer-uri constant://?val=jsonl://?writer=stdout:// -iterator-uri ${ITERATOR}"
@@ -49,25 +64,46 @@ do
     FEATURES_ARGS="${FEATURES_ARGS} ${SRC}"
 done
 
-echo "wof-tippecanoe-features ${FEATURES_ARGS} | tippecanoe -P -z ${ZOOM} -pf -pk -o /usr/local/data/${NAME}.mbtiles"
+echo "wof-tippecanoe-features ${FEATURES_ARGS} | tippecanoe -P -z ${ZOOM} -pf -pk -o /usr/local/data/${NAME}.pmtiles"
 
-# wof-tippecanoe-features ${FEATURES_ARGS} | tippecanoe -P -z ${ZOOM} -pf -pk -o /usr/local/data/${NAME}.mbtiles
+if [ "${WRITE_FEATURES}" = "1" ]
+then
+    
+    wof-tippecanoe-features ${FEATURES_ARGS} > /usr/local/data/features.jsonl
+    
+    COUNT=`cat /usr/local/data/features.jsonl | wc -l`
+    echo "Wrote ${COUNT} features"
+    
+    if [ $? -ne 0 ]
+    then
+	echo "Failed to write all features"
+	exit 1;
+    fi
+    
+    tippecanoe -P -z ${ZOOM} -pf -pk -o /usr/local/data/${NAME}.pmtiles /usr/local/data/features.jsonl
 
-wof-tippecanoe-features ${FEATURES_ARGS} > /usr/local/data/features.jsonl
+    # pmtiles convert -tmpdir /usr/local/data /usr/local/data/${NAME}.mbtiles /usr/local/data/${NAME}.pmtiles
+else 
+    wof-tippecanoe-features ${FEATURES_ARGS} | tippecanoe -P -z ${ZOOM} -pf -pk -o /usr/local/data/${NAME}.pmtiles
+fi
 
-COUNT=`cat /usr/local/data/features.jsonl | wc -l`
-echo "${COUNT} features"
+if [ $? -ne 0 ]
+then
 
-tippecanoe -P -z ${ZOOM} -pf -pk -o /usr/local/data/${NAME}.mbtiles /usr/local/data/features.jsonl
-
-pmtiles convert -tmpdir /usr/local/data /usr/local/data/${NAME}.mbtiles /usr/local/data/${NAME}.pmtiles
-
-# rm -f /usr/local/data/${NAME}.mbtiles
+    echo "Failed to create PMTiles database"
+    exit 1
+fi
 
 if [ "${TARGET}" != "" ]
 then
+    
     copy-uri -source-uri file:///usr/local/data/${NAME}.pmtiles -target-uri ${TARGET}
-    copy-uri -source-uri file:///usr/local/data/${NAME}.mbtiles -target-uri ${TARGET}
-    copy-uri -source-uri file:///usr/local/data/features.jsonl -target-uri ${TARGET}        
+    # copy-uri -source-uri file:///usr/local/data/${NAME}.mbtiles -target-uri ${TARGET}
+
+    if [ -f /usr/local/data/features.jsonl ]
+    then
+	copy-uri -source-uri file:///usr/local/data/features.jsonl -target-uri ${TARGET}
+    fi
 fi
 
+exit 0
