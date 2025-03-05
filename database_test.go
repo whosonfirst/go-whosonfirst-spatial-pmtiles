372ccc3c6b41cd0130e536e1650d48697aeb5d54
sfomuseumbot <sfomuseumbot@localhost>
2025-03-05 14:53:44 -0800

    block out intersects test

--- database_test.go
+++ database_test.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"path/filepath"
 	"strings"
-	_ "log/slog"
-
+	"log/slog"
+	"io"
+	"os"
+	
 	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/geojson"	
 	"github.com/whosonfirst/go-whosonfirst-spatial/database"
 	"github.com/whosonfirst/go-whosonfirst-spatial/filter"
 )
@@ -114,5 +117,75 @@ func TestPointInPolygon(t *testing.T) {
 }
 
 func TestIntersects(t *testing.T) {
-	t.Skip()
+	
+	rel_path := "fixtures/sf.pmtiles"
+	abs_path, err := filepath.Abs(rel_path)
+
+	if err != nil {
+		t.Fatalf("Failed to derive absolute path for %s, %v", rel_path, err)
+	}
+
+	root := filepath.Dir(abs_path)
+	fname := filepath.Base(abs_path)
+
+	fname = strings.Replace(fname, ".pmtiles", "", 1)
+	
+	db_uri := fmt.Sprintf("pmtiles://?tiles=file://%s&database=%s&zoom=13&enable_cache=true&layer=whosonfirst", root, fname)
+	
+	ctx := context.Background()
+	
+	db, err := database.NewSpatialDatabase(ctx, db_uri)
+	
+	if err != nil {
+		t.Fatalf("Failed to create new spatial database for %s, %v", db_uri, err)
+	}
+
+	defer db.Disconnect(ctx)
+
+	r, err := os.Open("fixtures/1108830809.geojson")
+
+	if err != nil {
+		t.Fatalf("Failed to open 1108830809 for reading, %v", err)
+	}
+
+	defer r.Close()
+
+	body, err := io.ReadAll(r)
+
+	if err != nil {
+		t.Fatalf("Failed to read 1108830809, %v", err)
+	}
+
+	f, err := geojson.UnmarshalFeature(body)
+
+	if err != nil {
+		t.Fatalf("Failed to unmarshal 1108830809, %v", err)
+	}
+
+	geom := f.Geometry
+	
+	rsp, err := db.Intersects(ctx, geom)
+
+	if err != nil {
+		t.Fatalf("Failed to perform intersects query, %v", err)
+	}
+
+	results := rsp.Results()
+	count := len(results)
+
+	/*
+	expected := 9
+
+	if count != expected {
+		t.Fatalf("Unexpected count (%d), expected %d", count, expected)
+	}
+	*/
+
+	slog.Info("count", "c", count)
+	
+	for _, r := range results {
+		slog.Info("r", "id", r.Id(), "name", r.Name())
+	}
+
+	
 }
